4c6d2bf29507df790fce2345137f8c28462b0f3c
Sabithasan2098 <sabithasan2098@gmail.com>
2024-02-02 12:41:58 +0600

    post a user

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,23 @@
 import express, { Application, Request, Response } from 'express'
 const app: Application = express()
 import cors from 'cors'
-
+import userService from './app/modules/users/user.service'
+import userRouter from './app/modules/users/user.route'
 // parse data --> middleware
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // test
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
+app.use('/api/v1/users', userRouter.router)
+
+app.get('/', async (req: Request, res: Response) => {
+  await userService.createUser({
+    id: '999',
+    role: 'student',
+    password: '1234',
+  })
+  res.send(`Hello developers don't worry server is running`)
 })
 
 export default app
