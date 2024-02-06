b26689024d99c6ca7de821ff7cf382c90a0510dc
Sabithasan2098 <sabithasan2098@gmail.com>
2024-02-06 13:09:25 +0600

    Zod error

--- src/app/middleware/globalErrorHandelar.ts
+++ src/app/middleware/globalErrorHandelar.ts
@@ -1,29 +1,36 @@
 /* eslint-disable no-console */
 /* eslint-disable no-unused-expressions */
-import { ErrorRequestHandler } from 'express'
-import config from '../../config'
-import { IGenericErrorMessage } from '../../interfaces/error'
-import handleValidationError from '../../error/handleValidationError'
-import ApiError from '../../error/apiError'
-import { errorLogger } from '../../shered/logger'
+import { ErrorRequestHandler } from 'express';
+import config from '../../config';
+import { IGenericErrorMessage } from '../../interfaces/error';
+import handleValidationError from '../../error/handleValidationError';
+import ApiError from '../../error/apiError';
+import { errorLogger } from '../../shered/logger';
+import { ZodError } from 'zod';
+import handleZodError from '../../error/handleZodError';
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   config.env === 'development'
     ? console.log(`globalErrorhandler ~~`, err)
-    : errorLogger.error(`globalErrorHandler ~~`, err)
+    : errorLogger.error(`globalErrorHandler ~~`, err);
 
-  let statusCode: number = 500
-  let message: string = 'something went wrong !'
-  let errorMessage: IGenericErrorMessage[] = []
+  let statusCode: number = 500;
+  let message: string = 'something went wrong !';
+  let errorMessage: IGenericErrorMessage[] = [];
 
   if (err.name === 'ValidationError') {
-    const simplifiedError = handleValidationError(err)
-    statusCode = simplifiedError.statusCode
-    message = simplifiedError.message
-    errorMessage = simplifiedError.errorMessage
+    const simplifiedError = handleValidationError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessage = simplifiedError.errorMessage;
+  } else if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessage = simplifiedError.errorMessage;
   } else if (err instanceof ApiError) {
-    statusCode = err?.statusCode
-    message = err.message
+    statusCode = err?.statusCode;
+    message = err.message;
     errorMessage = err?.message
       ? [
           {
@@ -31,9 +38,9 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
             message: err?.message,
           },
         ]
-      : []
+      : [];
   } else if (err instanceof Error) {
-    message = err?.message
+    message = err?.message;
     errorMessage = err?.message
       ? [
           {
@@ -41,7 +48,7 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
             message: err?.message,
           },
         ]
-      : []
+      : [];
   }
 
   res.status(statusCode).json({
@@ -49,9 +56,9 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     message,
     errorMessage,
     stack: config.env !== 'production' ? err?.stack : undefined,
-  })
+  });
 
-  next()
-}
+  next();
+};
 
-export default globalErrorHandler
+export default globalErrorHandler;
--- src/app/modules/users/user.controller.ts
+++ src/app/modules/users/user.controller.ts
@@ -1,28 +1,19 @@
-import { RequestHandler } from 'express'
-import { userService } from './user.service'
+import { RequestHandler } from 'express';
+import { userService } from './user.service';
 // import { z } from 'zod'
 
 const createUser: RequestHandler = async (req, res, next) => {
   try {
-    // using zod
-    // const createUserZodSchema = z.object({
-    //   body: z.object({
-    //     role: z.string({ required_error: 'role is required' }),
-    //     password: z.string().optional(),
-    //   }),
-    // })
-    // await createUserZodSchema.parseAsync(req)
-    // -----------------------------------------------------//
-    const { user } = req.body
-    const result = await userService.createUser(user)
+    const { user } = req.body;
+    const result = await userService.createUser(user);
     res.status(200).json({
       success: true,
       message: 'creating user successfully',
       data: result,
-    })
+    });
   } catch (err) {
-    next(err)
+    next(err);
   }
-}
+};
 
-export const userController = { createUser }
+export const userController = { createUser };
