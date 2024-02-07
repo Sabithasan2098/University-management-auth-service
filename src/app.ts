ba31ada2756b757b9c347221a7926f72e2e5cbae
Sabithasan2098 <sabithasan2098@gmail.com>
2024-02-07 08:37:04 +0600

    [Done]controller & route also update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,21 @@
-import express, { Application, NextFunction, Request, Response } from 'express';
+import express, { Application } from 'express';
 const app: Application = express();
 import cors from 'cors';
 import globalErrorHandler from './app/middleware/globalErrorHandelar';
 import { userRoutes } from './app/modules/users/user.route';
+import { academicSemesterRoutes } from './app/modules/academicSemister/academicSemester.route';
 // parse data --> middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/academicSemester', academicSemesterRoutes);
 
 // test
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-  throw new Error('Testing errorlogger');
-});
+// app.get('/', (req: Request, res: Response, next: NextFunction) => {
+//   throw new Error('Testing errorlogger');
+// });
 // global error handler
 app.use(globalErrorHandler);
 
