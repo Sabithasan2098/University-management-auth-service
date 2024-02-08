8216f93533afb018f399b43c3ffe11ed615391fd
Sabithasan2098 <sabithasan2098@gmail.com>
2024-02-08 07:51:14 +0600

    [Done] successfuly match semester title & code

--- src/app/modules/academicSemister/academicSemester.service.ts
+++ src/app/modules/academicSemister/academicSemester.service.ts
@@ -1,9 +1,18 @@
+import ApiError from '../../../error/apiError';
+import { academicSemesterTitleCodeMapper } from './academicSemester.constant';
 import { IAcademicSemester } from './academicSemester.interface';
 import { academicSemester } from './academicSemesterSchema&model';
+import status from 'http-status';
 
 const createAcademicSemester = async (
   payload: IAcademicSemester,
 ): Promise<IAcademicSemester> => {
+  if (academicSemesterTitleCodeMapper[payload.title] !== payload.code) {
+    throw new ApiError(
+      status.BAD_REQUEST,
+      'Invalid semester name or semester code',
+    );
+  }
   const result = await academicSemester.create(payload);
   return result;
 };
